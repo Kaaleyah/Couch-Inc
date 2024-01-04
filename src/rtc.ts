188364f45327923afbf316935c94de8d19884d50
Furkan Can Baytemur <41757315+Kaaleyah@users.noreply.github.com>
2024-01-04 16:55:57 +0300

    Video is streamed to remote peer

--- src/rtc.ts
+++ src/rtc.ts
@@ -5,8 +5,11 @@ const configuration = { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] }
 const peerConnection = new RTCPeerConnection(configuration);
 export var controlChannel: RTCDataChannel;
 
+
+// Peer A
 export async function sendOffer(memberID: string, socket: any) {
     controlChannel = peerConnection.createDataChannel('videoControls');
+    console.log("creating offer");
 
     controlChannel.onopen = () => {
         console.log('Control channel is open');
@@ -46,9 +49,11 @@ export async function sendOffer(memberID: string, socket: any) {
     listenForAnswer(socket, peerConnection);
 }
 
+// Peer B
 export async function listenForOffer(socket: any) {
     socket.on("offerSDP", (senderID: string, offerSDP: any) => {
         console.log(`Offer SDP: ${JSON.stringify(offerSDP)}`);
+        listenForVideo()
 
         peerConnection.ondatachannel = (event) => {
             controlChannel = event.channel;
@@ -92,6 +97,7 @@ export async function listenForOffer(socket: any) {
     })
 }
 
+// Peer A
 export async function listenForAnswer(socket: any, peerConnection: RTCPeerConnection) {
     socket.on("answerSDP", (senderID: string, answerSDP: any) => {
         console.log(`Answer SDP: ${JSON.stringify(answerSDP)}`);
@@ -99,6 +105,51 @@ export async function listenForAnswer(socket: any, peerConnection: RTCPeerConnec
     })
 }
 
-export async function listenToVideoControls() {
+export async function listenForVideo() {
+    console.log('Listening for video');
 
+    peerConnection.ontrack = (event) => {
+        console.log('Track event');
+        let videoComponent = document.createElement('video')
+        videoComponent!.id = 'videoComponent'
+
+        let videoContainer = document.querySelector<HTMLDivElement>('#video')
+        videoContainer!.innerHTML = ''
+        videoContainer?.appendChild(videoComponent!)
+
+        videoComponent!.srcObject = event.streams[0];
+        videoComponent!.setAttribute('controls', 'true')
+    }
+}
+
+export async function sendVideo(video: HTMLVideoElement) {
+    console.log('Sending video');
+    video.play();
+
+    const stream = (video as any).captureStream({ audio: true, video: true })
+    
+    video.addEventListener('loadedmetadata', () => {
+        const mediaStream = (video as any).captureStream({ audio: true, video: true })
+    
+        mediaStream.getTracks().forEach(track => {
+            peerConnection.addTrack(track, stream);
+        });
+
+        
+    });
+
+    /*
+    navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+        console.log(stream);
+        
+        stream.getTracks().forEach(track => {
+            peerConnection.addTrack(track, stream);
+        });
+    })
+    .catch(error => {
+        // Handle error accessing camera
+    });
+    */
+    
 }
